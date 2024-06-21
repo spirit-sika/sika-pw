b1fc658a7483aacf4f266f6985bbe527e2ab8929
spirit223 <2235569493@qq.com>
2024-06-21 16:27:52 +0800

    将错误常量转移到异常类中

--- sika-pw-common/src/main/java/cc/sika/exception/CaptchaException.java
+++ sika-pw-common/src/main/java/cc/sika/exception/CaptchaException.java
@@ -1,6 +1,15 @@
 package cc.sika.exception;
 
-public class CaptchaException extends RuntimeException {
+public class CaptchaException extends SikaRuntimeException {
+
+    public static final String NON_KEY = "非法的验证码键";
+    public static final String ERROR_CODE = "验证码错误";
+    public static final String EXPIRED_CODE = "验证码已过期";
+
+    public CaptchaException() {
+        super(ERROR_CODE);
+    }
+
     public CaptchaException(String message) {
         super(message);
     }
