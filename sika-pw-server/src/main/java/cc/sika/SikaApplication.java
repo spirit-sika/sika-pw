6341de7f9560b9071a3ef66af25655db02a88727
spirit223 <2235569493@qq.com>
2024-06-21 16:32:31 +0800

    配置包扫描

--- sika-pw-server/src/main/java/cc/sika/SikaApplication.java
+++ sika-pw-server/src/main/java/cc/sika/SikaApplication.java
@@ -1,9 +1,11 @@
 package cc.sika;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cc.sika.mapper")
 public class SikaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SikaApplication.class, args);
