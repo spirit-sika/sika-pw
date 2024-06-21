4fedd3b32a2945a34a38ae9cec1ae52de0b507a7
spirit223 <2235569493@qq.com>
2024-06-21 16:26:10 +0800

    单元测试用例: 用户mapper测试

---
+++ sika-pw-server/src/test/java/cc/sika/TestSikaUserMapper.java
@@ -0,0 +1,58 @@
+package cc.sika;
+
+import cc.sika.mapper.SikaUserMapper;
+import cc.sika.po.SikaUser;
+import cn.dev33.satoken.secure.SaSecureUtil;
+import cn.hutool.captcha.generator.RandomGenerator;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@SpringBootTest
+public class TestSikaUserMapper {
+
+    @Resource
+    private SikaUserMapper sikaUserMapper;
+
+    @Test
+    void insertTest() {
+        SikaUser user = SikaUser.builder()
+                .nickname("测试添加账号")
+                .password(SaSecureUtil.sha256("123456"))
+                .phoneNumber(phoneBuilder())
+                .email("123456@qq.com")
+                .sex((byte) 0)
+                .createBy("单元测试").build();
+        int result = sikaUserMapper.insert(user);
+        assert result == 1;
+        assert user.getUserId() != null && user.getUserId() > 0;
+    }
+
+    @Test
+    void loginTest() {
+        SikaUser sikaUser = sikaUserMapper.selectByPhoneAndPassword("11111111111", SaSecureUtil.sha256("123456"));
+        assert sikaUser != null;
+        assert sikaUser.getUserId() > 0;
+    }
+
+    @Test
+    void phoneQueryTest() {
+        // 如果存在多条记录, MyBatis会报错 org.mybatis.spring.MyBatisSystemException
+        SikaUser sikaUser = sikaUserMapper.selectByPhone("11111111111");
+        assert sikaUser != null;
+        assert sikaUser.getUserId() > 0;
+    }
+
+    private String phoneBuilder() {
+        List<String> existPhones = sikaUserMapper.selectAllPhoneNumber();
+        RandomGenerator randomGenerator = new RandomGenerator(StandardCharsets.UTF_8.name(), 11);
+        String randomNumber = randomGenerator.generate();
+        while (existPhones.contains(randomNumber)) {
+            randomNumber = randomGenerator.generate();
+        }
+        return randomNumber;
+    }
+}
