5f73e5757e3dcb8ed08756fdd0c148b8fdb69a0c
spirit223 <2235569493@qq.com>
2024-06-21 16:26:39 +0800

    用户查询注册相关mapper接口

---
+++ sika-pw-server/src/main/java/cc/sika/mapper/SikaUserMapper.java
@@ -0,0 +1,16 @@
+package cc.sika.mapper;
+
+import cc.sika.po.SikaUser;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SikaUserMapper {
+    SikaUser selectByPhoneAndPassword(@Param("phoneNumber") String phoneNumber, @Param("password") String password);
+
+    SikaUser selectByPhone(@Param("phoneNumber") String phoneNumber);
+
+    int insert(SikaUser sikaUser);
+
+    List<String> selectAllPhoneNumber();
+}
